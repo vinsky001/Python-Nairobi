ec15afb2ddeb7cca5e073cbaab542ab6f072f0a9
vinsky001 <ekibet544@gmail.com>
2023-04-26 18:15:12 +0300

    A program that uses itunes API to print a list of songs for a particular artist

---
+++ request_import.py
@@ -0,0 +1,19 @@
+import requests
+import sys
+import json
+
+if len(sys.argv) != 2:
+    sys.exit()
+    
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=100&term=" + sys.argv[1])
+
+
+o = response.json()
+for result in o[ "results"]:
+    print(result["trackName"])    
+    
+    
+    
+    
+    
+    
